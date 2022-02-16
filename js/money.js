eb6062a5b79fd44a2e0b96ccf0a58526e0e2ba60
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-02-17 00:57:47 +0600

    all sum done

--- js/money.js
+++ js/money.js
@@ -1,3 +1,13 @@
-document.getElementById('button-color').addEventListener('click', function () {
-
-})
+const btnCalculate = document.getElementById('calculate');
+const income = document.getElementById('income');
+const food = document.getElementById('food');
+const rent = document.getElementById('rent');
+const clothes = document.getElementById('clothees');
+const totoalExprese = document.getElementById('totoal-exprese');
+const balance = document.getElementById('balance');
+const remBalance = document.getElementById('rem-balance');
+const persent = document.getElementById('persent');
+const save = document.getElementById('save');
+const saveAmount = document.getElementById('save-amount');
+const Remaining = document.getElementById('rem-balance');
+const allInput = parseInt(document.querySelectorAll('input'));
